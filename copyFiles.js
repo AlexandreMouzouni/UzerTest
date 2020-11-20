b5a8fff0589f996c18775b9fa2690fa9e490aae7
AlexandreMouzouni <58190342+AlexandreMouzouni@users.noreply.github.com>
2020-11-20 20:41:49 +0100

    Create copyFiles.js

---
+++ copyFiles.js
@@ -0,0 +1,20 @@
+const BucketName = 'montri-prod.appspot.com';
+const srcFilename = 'NCA/preResources';
+const destFilename = 'NCA/resources';
+
+// Imports the Google Cloud client library
+const {Storage} = require('@google-cloud/storage');
+
+// Creates a client
+const storage = new Storage();
+
+async function copyFile() {
+  // Copies the files in the folder to the same bucket
+  await storage.bucket(BucketName).file(srcFilename).copy(destFilename);
+
+  console.log(
+    `gs://${BucketName}/${srcFilename} copied to gs://${BucketName}/${destFilename}.`
+  );
+}
+
+copyFile().catch(console.error);
